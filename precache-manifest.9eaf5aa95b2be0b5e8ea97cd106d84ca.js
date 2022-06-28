be6b6bc68602204d5dad088796887ab81a7b362f
shikang <shikang.n@gmail.com>
2022-06-28 13:52:45 +0800

    react with unity

---
+++ precache-manifest.9eaf5aa95b2be0b5e8ea97cd106d84ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b1b1f5cd339a77ec025",
+    "url": "/static/css/main.aea206f5.chunk.css"
+  },
+  {
+    "revision": "7b1b1f5cd339a77ec025",
+    "url": "/static/js/main.7b1b1f5c.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "309eee853e7b9478152b",
+    "url": "/static/js/2.309eee85.chunk.js"
+  },
+  {
+    "revision": "36777d478a7e515f8b01",
+    "url": "/static/js/runtime~main.36777d47.js"
+  },
+  {
+    "revision": "f0b7318e1ceaa3ec8fcca295076db09e",
+    "url": "/index.html"
+  }
+];
